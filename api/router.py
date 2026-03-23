137014aab004db3b78cff56eb253a80214ccd1d1
moshhhka <ulio_akio@mail.ru>
2026-03-24 00:14:23 +0300

    исправила файлы

--- api/router.py
+++ api/router.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Root API router composition."""
 
 from fastapi import APIRouter
@@ -7,13 +6,3 @@ from api.notifications import router as notifications_router
 
 api_router = APIRouter()
 api_router.include_router(notifications_router)
-=======
-"""Root API router composition."""
-
-from fastapi import APIRouter
-
-from api.notifications import router as notifications_router
-
-api_router = APIRouter()
-api_router.include_router(notifications_router)
->>>>>>> 61823b5f29fbd26f1ee9033a50b581cc288c6e36
--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 """Pydantic schema package for API contracts."""
-=======
-"""Pydantic schema package for API contracts."""
->>>>>>> 61823b5f29fbd26f1ee9033a50b581cc288c6e36
